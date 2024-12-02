0ce707244bd4ac6e66304bcb3a6d6afacc3a3ca0
yuanze31 <1538981620@qq.com>
2024-12-02 17:39:59 +0800

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# yuanze31 status
- Just for myself
+# Yuanze31 Status
+
+只是个人的[状态网页](https://status.yuanze31.xyz)套壳
+
+完全没有别的功能
---
+++ app/src/main/java/com/yuanze31/status/MainActivity.java
@@ -0,0 +1,40 @@
+package com.yuanze31.status;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            WebView MainWebView = findViewById(R.id.main_WebView);
+
+            setUpWebView(MainWebView);
+
+            return insets;
+        });
+    }
+
+
+    void setUpWebView(WebView webView) {
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://status.yuanze31.xyz");
+    }
+}
